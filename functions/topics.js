5d6c77b384698256b28dc60b3c90b46fda59bee3
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-08-08 18:29:32 -0300

    added server-side logic for netlify

---
+++ functions/topics.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+// const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+
+exports.handler = async (event, context) => {
+    const res = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3000/',
+        },
+        body: '',
+    };
+
+    const req = JSON.parse(event.body); // Parse the request body
+    const message = req.message;
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            response_format: { type: "json_object" },
+            messages: [{
+                role: "system",
+                content: `I ask you a finance question and you provide JSON object with up to 5 topics for my 2 Person podcast in Spanish: 
+                    {
+                        "topics": [
+                            {
+                                "name": "title of the topic",
+                                "description": "quick description of the topic in quistion",
+                            },
+                            "topic": [
+                            {
+                                "name": "title of the topic",
+                                "description": "quick description of the topic in quistion",
+                            },
+                        ]
+                    }`,
+            },
+            { role: "user", message },
+            ],
+            max_tokens: 600,
+        })
+    };
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json();
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+    return res;
+}
+
+
+// Local server testing
+// const PORT = 8000
+// const express = require('express');
+// const cors = require('cors');
+// const axios = require('axios');
+// require('dotenv').config();
+
+// const app = express()
+// app.use(express.json())
+// app.use(cors());
+
+// app.post('/topics', async (req, res) => {
+
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             model: 'gpt-3.5-turbo',
+//             response_format: { type: "json_object" },
+//             messages: [{
+//                 role: "system",
+//                 content: `I ask you a finance question and you provide JSON object with up to 5 topics for my 2 Person podcast in Spanish: 
+//                     {
+//                         "topics": [
+//                             {
+//                                 "name": "title of the topic",
+//                                 "description": "quick description of the topic in quistion",
+//                             },
+//                             "topic": [
+//                             {
+//                                 "name": "title of the topic",
+//                                 "description": "quick description of the topic in quistion",
+//                             },
+//                         ]
+//                     }`,
+//             },
+//             { role: "user", content: req.body.message },
+//             ],
+//             max_tokens: 600,
+//         })
+//     };
+
+//     try {
+//         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+//         const data = await response.json();
+//         res.send(data)
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })
+
+// app.listen(PORT, () => console.log('your server is running on PORT ' + PORT))
+
