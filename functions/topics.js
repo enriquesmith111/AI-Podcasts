760294a189a479a983efd37c5cf5859d456e6508
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-08-08 18:43:22 -0300

    test netlify function

--- functions/topics.js
+++ functions/topics.js
@@ -19,7 +19,6 @@ exports.handler = async (event, context) => {
         },
         body: '',
     };
-
     const req = JSON.parse(event.body); // Parse the request body
     const message = req.message;
 
