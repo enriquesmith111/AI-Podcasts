a9779f7cf62d198955c85f920996fba9ff816a73
Enrique Smith Dean <enriquesmith111@gmail.com>
2024-08-08 20:05:04 -0300

    fetch from netlify server added in app.js

--- functions/topics.js
+++ functions/topics.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // app.use(cors({ origin: 'http://localhost:3000/' }));
 
 exports.handler = async (event, context) => {
-    const req = JSON.parse(event.body); // Parse the request body
+    const req = event; // Parse the request body
     const message = req.message;
 
     const res = {
